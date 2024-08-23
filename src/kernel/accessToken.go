2842b34eee2693213aaa593b2295be2b8dc95542
Matrix-X <matrix-x@aritsan-cloud.com>
2024-08-23 23:38:36 +0800

    fix(log): http_debug log in powerlib

--- src/kernel/accessToken.go
+++ src/kernel/accessToken.go
@@ -86,7 +86,6 @@ func NewAccessToken(app ApplicationInterface) (*AccessToken, error) {
 }
 
 func (accessToken *AccessToken) GetToken(ctx context.Context, refresh bool) (resToken *response2.ResponseGetToken, err error) {
-
 	cacheKey := accessToken.GetCacheKey()
 	cache := accessToken.GetCache()
 
@@ -151,7 +150,6 @@ func (accessToken *AccessToken) SetToken(token *response2.ResponseGetToken) (tok
 		return nil, errors.New("failed to cache access token")
 	}
 	return accessToken, err
-
 }
 
 func (accessToken *AccessToken) getFormatToken(token object.HashMap) (*response2.ResponseGetToken, error) {
@@ -161,7 +159,6 @@ func (accessToken *AccessToken) getFormatToken(token object.HashMap) (*response2
 
 	if accessToken.TokenKey == "access_token" && token["access_token"] != nil {
 		resToken.AccessToken = token[accessToken.TokenKey].(string)
-
 	} else if accessToken.TokenKey == "component_access_token" && token["component_access_token"] != nil {
 		resToken.AccessToken = token[accessToken.TokenKey].(string)
 		resToken.ComponentAccessToken = token[accessToken.TokenKey].(string)
@@ -198,7 +195,6 @@ func (accessToken *AccessToken) Refresh(ctx context.Context) contract.AccessToke
 }
 
 func (accessToken *AccessToken) requestToken(ctx context.Context, credentials *object.StringMap) (*response2.ResponseGetToken, error) {
-
 	res, err := accessToken.sendRequest(ctx, credentials)
 	if err != nil {
 		return nil, err
@@ -218,7 +214,6 @@ func (accessToken *AccessToken) requestToken(ctx context.Context, credentials *o
 }
 
 func (accessToken *AccessToken) ApplyToRequest(request *http.Request, requestOptions *object.HashMap) (*http.Request, error) {
-
 	// query Access Token power
 	mapToken, err := accessToken.getQuery(request.Context())
 	if err != nil {
@@ -234,7 +229,6 @@ func (accessToken *AccessToken) ApplyToRequest(request *http.Request, requestOpt
 }
 
 func (accessToken *AccessToken) sendRequest(ctx context.Context, credential *object.StringMap) (*response2.ResponseGetToken, error) {
-
 	key := "json"
 	if accessToken.RequestMethod == http.MethodGet {
 		key = "query"
@@ -294,7 +288,6 @@ func (accessToken *AccessToken) SetCacheKey(key string) {
 // 2. 计算字符串的md5。"testappidtestsecret"的md5值为"edc5f6181730baffc0b88cf96658aeff"
 // 3. 加上PowerWeChat前缀命名空间："powerwechat.access_token."，最终结果为："powerwechat.access_token.edc5f6181730baffc0b88cf96658aeff"
 func (accessToken *AccessToken) GetCacheKey() string {
-
 	cacheKey := ""
 	if accessToken.CacheTokenKey != "" {
 		cacheKey = accessToken.CacheTokenKey
@@ -312,7 +305,7 @@ func (accessToken *AccessToken) GetDefaultCacheKey() string {
 	cacheKey := accessToken.CachePrefix + hex.EncodeToString(buffer[:])
 
 	// tbf
-	//fmt2.Dump(cacheKey)
+	// fmt2.Dump(cacheKey)
 
 	return cacheKey
 }
@@ -336,11 +329,9 @@ func (accessToken *AccessToken) getQuery(ctx context.Context) (*object.StringMap
 	}
 
 	return arrayReturn, err
-
 }
 
 func (accessToken *AccessToken) RegisterHttpMiddlewares() {
-
 	// log
 	logMiddleware := accessToken.GetMiddlewareOfLog
 
@@ -360,7 +351,6 @@ func (accessToken *AccessToken) OverrideGetMiddlewareOfLog() {
 	accessToken.GetMiddlewareOfLog = func(logger contract2.LoggerInterface) contract3.RequestMiddleware {
 		return func(handle contract3.RequestHandle) contract3.RequestHandle {
 			return func(request *http.Request) (response *http.Response, err error) {
-
 				logger := logger.WithContext(request.Context())
 
 				// 此处请求前后日志根据 log 配置中的 level 判断是否打印
@@ -380,7 +370,6 @@ func (accessToken *AccessToken) OverrideGetMiddlewareOfLog() {
 }
 
 func (accessToken *AccessToken) OverrideGetEndpoint() {
-
 	accessToken.GetEndpoint = func() (string, error) {
 		if accessToken.EndpointToGetToken == "" {
 			return "", errors.New("no endpoint for access token request")
@@ -388,7 +377,6 @@ func (accessToken *AccessToken) OverrideGetEndpoint() {
 
 		return accessToken.EndpointToGetToken, nil
 	}
-
 }
 
 func (accessToken *AccessToken) getTokenKey() string {
