08bf9b420783e95e81ce1007b2b68ee15a198d05
syd <syd@xibao100.com>
2024-10-12 15:31:16 +0800

    chore: followup upstream

--- test/featureUnit/work_externalcontact_del_contact_way_test.go
+++ test/featureUnit/work_externalcontact_del_contact_way_test.go
@@ -1,13 +1,14 @@
 package featureUnit
 
 import (
-	"github.com/ArtisanCloud/PowerLibs/v3/fmt"
+	"context"
 	"testing"
+
+	"github.com/ArtisanCloud/PowerLibs/v3/fmt"
 )
 
 func Test_ExternalContact_Del_Contact_Way(t *testing.T) {
-
-	response, _ := Work.ExternalContactContactWay.Delete("f3626f74a7f94784115b0b8a729c471f")
+	response, _ := Work.ExternalContactContactWay.Delete(context.Background(), "f3626f74a7f94784115b0b8a729c471f")
 
 	if response == nil {
 		t.Error("response nil")
@@ -16,5 +17,4 @@ func Test_ExternalContact_Del_Contact_Way(t *testing.T) {
 	}
 
 	fmt.Dump(response)
-
 }
