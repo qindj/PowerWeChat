107c13478200a344517ed88343a45a09c4b7b972
Matrix-X <hbj418@gmail.com>
2025-01-01 16:40:53 +0800

    Revert "fix(kernel): call Server.Serve nil pointer fixed"

--- src/kernel/serverGuard.go
+++ src/kernel/serverGuard.go
@@ -139,9 +139,7 @@ func (serverGuard *ServerGuard) Serve(request *http.Request) (response *http.Res
 
 	response, err = validatedGuard.Resolve(request)
 
-	if response != nil {
-		logger.Info("Server response created:", "content", response.ContentLength)
-	}
+	logger.Info("Server response created:", "content", response.ContentLength)
 
 	return response, err
 }
@@ -436,7 +434,7 @@ func (serverGuard *ServerGuard) ParseMessage(content string, dataType string) (c
 	callback = &models.Callback{}
 
 	if len(content) <= 0 {
-		return callback, nil
+		return nil, errors.New("request body is empty")
 	}
 
 	if dataType == messages.DataTypeXML {
